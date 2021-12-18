ac07ec68a787593a729af75d86512309c33466c6
Claudio Rivera <me@claudiorivera.com>
2021-12-18 09:22:07 -0600

    🎉 Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+  export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+
+<h1>hi</h1>
+
+<style>
+</style>
