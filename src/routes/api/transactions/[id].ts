c7009dd0d69f80855f70d4b4a6ef259457b58c81
Claudio Rivera <me@claudiorivera.com>
2021-12-22 07:50:22 -0600

    🔥 Remove unused CRUD route

--- src/routes/api/transactions/[id].ts
+++ src/routes/api/transactions/[id].ts
@@ -3,18 +3,6 @@ import { mongooseConnect } from "$lib/mongooseConnect";
 import type { Transaction } from "$lib/types";
 import type { Request } from "@sveltejs/kit";
 
-export const get = async (req: Request): Promise<{ body: Transaction }> => {
-  await mongooseConnect();
-
-  const transaction = await TransactionModel.findOne({
-    _id: req.params.id,
-  });
-
-  return {
-    body: transaction,
-  };
-};
-
 export const del = async (req: Request): Promise<{ body: Transaction }> => {
   await mongooseConnect();
 
