5c369493d3e8e9f42b689c7cad65195bcc912c7f
Claudio Rivera <me@claudiorivera.com>
2022-02-11 20:47:20 -0600

    ♻️ Refactor form with validation

--- src/lib/components/AddTransaction.svelte
+++ src/lib/components/AddTransaction.svelte
@@ -1,63 +1,63 @@
 <script lang="ts">
+  import { createForm } from "svelte-forms-lib";
+  import * as yup from "yup";
   export let fetchTransactions: () => Promise<void>;
 
-  let description: string;
-  let amount: number;
-  let transactionType: string = "expense";
-  let inputRef: HTMLInputElement;
-
-  const transformAmount = (amount: number) =>
-    transactionType === "expense" ? -amount : amount;
-
-  const handleSubmit = async () => {
-    await fetch("/api/transactions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+  const { form, errors, isSubmitting, handleChange, handleSubmit } = createForm(
+    {
+      initialValues: {
+        description: "",
+        amount: 0,
+        transactionType: "EXPENSE",
       },
-      body: JSON.stringify({ description, amount: transformAmount(amount) }),
-    });
-    description = "";
-    amount = null;
-    inputRef.focus();
-    fetchTransactions();
-  };
+      validationSchema: yup.object().shape({
+        description: yup.string().required("Please enter a description"),
+        amount: yup.number().not([0], "Please enter an amount").required(),
+        transactionType: yup.string().oneOf(["EXPENSE", "INCOME"]),
+      }),
+
+      onSubmit: async ({ description, amount, transactionType }) => {
+        await fetch("/api/transactions", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            description,
+            amount,
+            transactionType,
+          }),
+        });
+        fetchTransactions();
+      },
+    },
+  );
 </script>
 
 <form
-  on:submit|preventDefault={handleSubmit}
+  on:submit={handleSubmit}
   class="shadow rounded m-1 p-1 bg-white flex flex-col"
 >
   <div class="flex flex-wrap justify-start m-3">
     <div class="flex-grow flex flex-col items-end">
       <div class="flex gap-5">
         <div class="flex items-center gap-1">
-          <input
-            id="transactionType-expense"
-            type="radio"
-            class="peer form-radio hidden"
-            bind:group={transactionType}
-            value="expense"
-          />
-          <label
-            for="transactionType-expense"
-            class="my-2 p-2 w-20 text-center bg-slate-400 rounded ring  ring-slate-400 bg-transparent peer-checked:bg-indigo-500 peer-checked:text-white peer-checked:ring-indigo-600 cursor-pointer"
-            >Expense</label
+          <select
+            name="transactionType"
+            id="transactionType"
+            on:change={handleChange}
+            bind:value={$form.transactionType}
           >
-        </div>
-        <div class="flex items-center gap-1">
-          <input
-            id="transactionType-income"
-            type="radio"
-            class="peer form-radio hidden"
-            bind:group={transactionType}
-            value="income"
-          />
-          <label
-            for="transactionType-income"
-            class="my-2 p-2 w-20 text-center bg-slate-400 rounded ring  ring-slate-400 bg-transparent peer-checked:bg-indigo-500 peer-checked:text-white peer-checked:ring-indigo-600 cursor-pointer"
-            >Income</label
-          >
+            <option
+              class="my-2 p-2 w-20 text-center bg-slate-400 rounded ring  ring-slate-400 bg-transparent peer-checked:bg-indigo-500 peer-checked:text-white peer-checked:ring-indigo-600 cursor-pointer"
+              value={"EXPENSE"}>Expense</option
+            >
+
+            <option
+              class="my-2 p-2 w-20 text-center bg-slate-400 rounded ring  ring-slate-400 bg-transparent peer-checked:bg-indigo-500 peer-checked:text-white peer-checked:ring-indigo-600 cursor-pointer"
+              value={"INCOME"}>Income</option
+            >
+          </select>
         </div>
       </div>
       <div class="flex items-center gap-1">
@@ -67,9 +67,11 @@
           type="text"
           name="description"
           placeholder="Description"
-          bind:value={description}
-          bind:this={inputRef}
+          bind:value={$form.description}
         />
+        {#if $errors.description}
+          <small>{$errors.description}</small>
+        {/if}
       </div>
       <div class="flex items-center gap-1">
         <label for="amount" class="mr-2">Amount</label>
@@ -77,17 +79,19 @@
           inputmode="decimal"
           class="form-input rounded my-2"
           type="number"
-          pattern={`^\d*(\.\d{(0, 2)})?$`}
           step="0.01"
           name="amount"
-          placeholder="0.00"
-          bind:value={amount}
+          bind:value={$form.amount}
         />
+        {#if $errors.amount}
+          <small>{$errors.amount}</small>
+        {/if}
       </div>
     </div>
   </div>
   <button
-    class="px-4 py-3 bg-indigo-500 rounded mx-auto my-3 w-1/2 text-white font-bold text-xl"
+    class="px-4 py-3 bg-indigo-500 disabled:bg-gray-500 rounded mx-auto my-3 w-1/2 text-white font-bold text-xl disabled:pointer-events-none"
+    disabled={$isSubmitting}
     type="submit">Submit</button
   >
 </form>
