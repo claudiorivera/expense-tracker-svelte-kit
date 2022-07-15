37c1a6f0c145c023d63efe0a2d4584526c3c6317
Claudio Rivera <claudio@foundrymakes.com>
2022-07-15 17:06:53 -0500

    👽️ Update method names

--- src/routes/api/transactions/[id].ts
+++ src/routes/api/transactions/[id].ts
@@ -2,7 +2,7 @@ import { TransactionModel } from "$lib/models/Transaction";
 import { mongooseConnect } from "$lib/mongooseConnect";
 import type { RequestEvent } from "@sveltejs/kit/types/internal";
 
-export const del = async ({ params }: RequestEvent) => {
+export const DELETE = async ({ params }: RequestEvent) => {
   await mongooseConnect();
 
   await TransactionModel.findOneAndDelete({
