6ae0b1fbfaa60d5b6f8b0ab3b7af2897f8d8c19e
Claudio Rivera <claudio@foundrymakes.com>
2022-07-15 17:10:36 -0500

    🐛 Fix Mongoose

--- src/lib/models/Transaction.ts
+++ src/lib/models/Transaction.ts
@@ -1,5 +1,6 @@
 import type { Transaction } from "$lib/types";
-import { model, models, Schema } from "mongoose";
+import mongoose from "mongoose";
+const { model, models, Schema } = mongoose;
 
 const TransactionSchema = new Schema<Transaction>({
   description: {
