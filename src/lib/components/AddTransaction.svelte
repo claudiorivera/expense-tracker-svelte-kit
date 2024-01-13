78a321c0e4d614ea6b4922f2e14ca9a4d318f8c5
Claudio Rivera <me@claudiorivera.com>
2024-01-13 10:44:12 -0600

    💄 Implement original styling

--- src/lib/components/AddTransaction.svelte
+++ src/lib/components/AddTransaction.svelte
@@ -19,9 +19,10 @@
 		resetForm: true,
 		autoFocusOnError: true,
 	}}
+	class="shadow rounded bg-white flex flex-col items-center gap-4 p-4"
 >
 	<Form.Field {config} name="transactionType">
-		<Form.Item>
+		<Form.Item class="self-end w-1/2">
 			<Form.Select
 				selected={{
 					value: TransactionType.EXPENSE,
@@ -38,22 +39,30 @@
 					>
 				</Form.SelectContent>
 			</Form.Select>
-			<Form.Validation />
 		</Form.Item>
 	</Form.Field>
+
 	<Form.Field {config} name="description">
-		<Form.Item>
+		<Form.Item class="flex items-center gap-2 self-end">
 			<Form.Label>Description</Form.Label>
-			<Form.Input autofocus />
-			<Form.Validation />
+			<div class="flex flex-col gap-1">
+				<Form.Input autofocus />
+				<Form.Validation class="h-2" />
+			</div>
 		</Form.Item>
 	</Form.Field>
+
 	<Form.Field {config} name="amount">
-		<Form.Item>
+		<Form.Item class="flex items-center gap-2 self-end">
 			<Form.Label>Amount</Form.Label>
-			<Form.Input type="number" step={0.01} />
-			<Form.Validation />
+			<div class="flex flex-col gap-1">
+				<Form.Input type="number" step={0.01} />
+				<Form.Validation class="h-2" />
+			</div>
 		</Form.Item>
 	</Form.Field>
-	<Form.Button>Submit</Form.Button>
+
+	<Form.Button class="bg-indigo-500 text-white font-semibold w-1/2"
+		>Submit</Form.Button
+	>
 </Form.Root>
