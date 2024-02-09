33a7fedf22db4ab7c29fd16419aec8004717d852
Claudio Rivera <me@claudiorivera.com>
2024-02-08 21:44:46 -0600

    🔧 Update postcss config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,13 +1,11 @@
-const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
+const tailwindcss = require("tailwindcss");
 
-const config = {
+module.exports = {
 	plugins: [
 		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss(),
 		//But others, like autoprefixer, need to run after,
-		autoprefixer,
+		autoprefixer(),
 	],
 };
-
-module.exports = config;
