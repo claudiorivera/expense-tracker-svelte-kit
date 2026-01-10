5ccb77e3f8bb6f3f40336fb0696e439ab32ffbe2
Claudio Rivera <me@claudiorivera.com>
2026-01-10 15:50:38 -0600

    💄 Fix spacing on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,6 @@
 import "../app.css";
 </script>
 
-<main class="cursor-default antialiased container py-10 mx-auto max-w-lg">
+<main class="cursor-default antialiased container py-10 px-4 mx-auto max-w-lg">
 	<slot />
 </main>
